c545eba42f684269d118f8a6acaff208b86df270
Javier Goldschmidt <javi@elgatoylacaja.com>
2020-05-09 13:15:03 -0300

    Dummy api

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return jsonify({"key": request.args.get("value")})
 
 if __name__ == '__main__':
     app.run(debug=True)
